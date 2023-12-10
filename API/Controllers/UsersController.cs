f3866a3435021e43676e96770d269b737a535b8b
jack <mitsuru.jack@gmail.com>
2023-12-10 13:39:18 +0900

    Replace current api with async one

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,7 +1,7 @@
-using System.Runtime.CompilerServices;
 using API.Data;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
@@ -16,14 +16,16 @@ public class UsersController : ControllerBase
     }
 
     [HttpGet]
-    public ActionResult<IEnumerable<AppUser>> GetUsers()
+    public async Task <ActionResult<IEnumerable<AppUser>>> GetUsers()
     {
-        return _context.Users.ToList();
+        var users = await _context.Users.ToListAsync();
+
+        return users;
     }
 
     [HttpGet("{id}")]
-    public ActionResult<AppUser> GetUser(int id)
+    public async Task <ActionResult<AppUser>> GetUser(int id)
     {
-        return _context.Users.Find(id);
+        return await _context.Users.FindAsync(id);
     }
 }
