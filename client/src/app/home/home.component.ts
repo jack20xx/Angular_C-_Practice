d659ccce2309d2381e33e9530bbe483b0bb26927
jack <mitsuru.jack@gmail.com>
2024-01-10 22:28:19 +0900

    Show members list on user page

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,4 +1,3 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -10,22 +9,13 @@ export class HomeComponent implements OnInit {
   registerMode = false;
   users: any;
 
-  constructor(private http: HttpClient){}
+  constructor(){}
 
   ngOnInit(): void {
-    this.getUsers();
   }
 
   public registerToggle() {
     this.registerMode = !this.registerMode;
-  }
-
-  public getUsers(){
-    this.http.get('https://localhost:5001/api/users').subscribe({
-      next: response => this.users = response,
-      error: error => console.log(error),
-      complete: () => console.log('Request has completed'),
-    })
   }
 
   public cancelRegisterMode(event: boolean){
