f2e39e5fc802fb2ddf405a23ebe78f9e1d996f70
jack <mitsuru.jack@gmail.com>
2024-01-20 20:47:26 +0900

    Create busy service to control show loading

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -2,18 +2,26 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { Member } from '../_models/member';
+import { map, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MembersService {
   baseUrl = environment.apiUrl;
+  members: Member[] = [];
 
   constructor(private http: HttpClient) { }
 
   public getMembers() {
+    if(this.members.length > 0) return of(this.members);
     return this.http
-      .get<Member[]>(this.baseUrl + 'users');
+      .get<Member[]>(this.baseUrl + 'users').pipe(
+        map(members => {
+          this.members = members;
+          return members;
+        })
+      );
   }
 
   public getMember(username: string) {
